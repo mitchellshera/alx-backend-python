58f62b3d52434dd355a48b600ebacbf155f9ce20
mitchellshera <mitchellwachera@gmail.com>
2023-12-09 00:11:59 +0300

    9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,21 +2,21 @@
 '''Task 9's module'''
 
 
-from typing import List, Tuple
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Returns a list of tuples where each tuple
-    contains an element from the input list
+    Returns a list of tuples where each
+    tuple contains an element from the input list
     and its corresponding length.
 
     Args:
         lst (List[str]): The input list of strings.
 
     Returns:
-        List[Tuple[str, int]]: A list of
-        tuples, where each tuple contains a string
+        List[Tuple[str, int]]: A list of tuples,
+        where each tuple contains a string
         from the input list and its length.
     """
     return [(i, len(i)) for i in lst]
