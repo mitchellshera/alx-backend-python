f29406691af556683268c9c0390c3ddca26be46e
mitchellshera <mitchellwachera@gmail.com>
2024-01-08 13:19:56 +0300

    trial

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,15 @@
 
 
 import unittest
+from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import utils
+
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
 
 class TestAccessNestedMap(unittest.TestCase):
     '''class for testing utils.py'''
@@ -14,8 +21,15 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
-        self.assertEqual(utils.access_nested_map(nested_map, path), expected_result)
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            ) -> None:
+        """Tests `access_nested_map`'s exception raising."""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
 
 if __name__ == '__main__':
     unittest.main()
