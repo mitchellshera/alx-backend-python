e855027101b75d5070597b8997794be44d4cc305
mitchellshera <mitchellwachera@gmail.com>
2024-01-08 13:23:12 +0300

    Refactor access_nested_map tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,33 +3,25 @@
 
 
 import unittest
-from typing import Dict, Tuple, Union
-from unittest.mock import patch, Mock
 from parameterized import parameterized
-
-from utils import (
-    access_nested_map,
-    get_json,
-    memoize,
-)
+from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
-    '''class for testing utils.py'''
-    
+
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": 1}, ["a"], 1),
+        ({"a": {"b": 2}}, ["a"], {"b": 2}),
+        ({"a": {"b": 2}}, ["a", "b"], 2),
     ])
-    def test_access_nested_map_exception(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            exception: Exception,
-            ) -> None:
-        """Tests `access_nested_map`'s exception raising."""
-        with self.assertRaises(exception):
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ["a"], KeyError),
+        ({"a": 1}, ["a", "b"], KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception):
+        with self.assertRaises(expected_exception) as context:
             access_nested_map(nested_map, path)
 
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(str(context.exception), f"{path[-1]}")
