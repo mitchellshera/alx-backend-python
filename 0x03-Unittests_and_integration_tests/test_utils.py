bd83be4996779749889030d299e6ef0fa560211f
mitchellshera <mitchellwachera@gmail.com>
2024-01-08 14:37:36 +0300

    pycode

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,7 @@ from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, Mock
 from typing import Dict, Tuple, Union
 
+
 class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
@@ -22,7 +23,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ["a"], KeyError, "'a'"),
         ({"a": 1}, ["a", "b"], KeyError, "'b'"),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_exception, expected_message):
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_exception, expected_message):
         with self.assertRaises(expected_exception) as context:
             access_nested_map(nested_map, path)
 
@@ -31,7 +33,7 @@ class TestAccessNestedMap(unittest.TestCase):
 
 class TestGetJson(unittest.TestCase):
     '''class for testing get_json function'''
-    
+
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
@@ -47,12 +49,13 @@ class TestGetJson(unittest.TestCase):
             self.assertEqual(get_json(test_url), test_payload)
             request_get.assert_called_once_with(test_url)
 
+
 class TestMemoize(unittest.TestCase):
     """Test case for the `memoize` decorator."""
 
     class TestClass:
         """Test class for memoized methods."""
-        
+
         def a_method(self):
             """A method returning the constant value 42."""
             return 42
@@ -79,5 +82,5 @@ class TestMemoize(unittest.TestCase):
         self.assertEqual(result2, 42)
 
 
-if  __name__ == '__main__':
+if __name__ == '__main__':
     unittest.main()
