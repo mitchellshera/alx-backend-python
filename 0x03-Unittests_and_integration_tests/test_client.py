9f11e0987d7b009ecb100ee86a2c730372c87d1b
mitchellshera <mitchellwachera@gmail.com>
2024-01-08 15:09:13 +0300

    4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
-'''module for testing client.py'''
-
+'''Module for testing client.py'''
 
 import unittest
-from unittest.mock import patch, MagicMock
-from parameterized import parameterized
+from unittest.mock import patch
 from client import GithubOrgClient
+from parameterized import parameterized
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    '''class for testing GithubOrgClient'''
-    
+    '''Class for testing GithubOrgClient'''
+
     @patch('client.get_json', return_value={'login': 'dummy_org'})
     @parameterized.expand([
         ('google',),
         ('abc',),
     ])
     def test_org(self, org_name):
+        '''Test the org method of GithubOrgClient'''
         with patch('client.get_json', return_value={'login': org_name}):
             client = GithubOrgClient(org_name)
             result = client.org()
