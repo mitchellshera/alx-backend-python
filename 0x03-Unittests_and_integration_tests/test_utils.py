8e01da27f5436fb71878a65d311a9e0d9594af74
mitchellshera <mitchellwachera@gmail.com>
2024-01-08 13:28:53 +0300

    Fix KeyError handling in access_nested_map function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,11 +17,15 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
-        ({}, ["a"], KeyError),
-        ({"a": 1}, ["a", "b"], KeyError),
+        ({}, ["a"], KeyError, "'a'"),
+        ({"a": 1}, ["a", "b"], KeyError, "'b'"),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_exception):
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception, expected_message):
         with self.assertRaises(expected_exception) as context:
             access_nested_map(nested_map, path)
 
-        self.assertEqual(str(context.exception), f"{path[-1]}")
+        self.assertEqual(str(context.exception), expected_message)
+
+
+if  __name__ == '__main__':
+    unittest.main()
